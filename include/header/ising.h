9c828ed167c2c7c5c4f37372c25a24c76ef087f0
seabornema <seabornem@proton.me>
2026-05-09 15:44:27 -0400

    Update ising.h

--- include/header/ising.h
+++ include/header/ising.h
@@ -6,17 +6,19 @@
 
 #include <random>
 #include <cmath>
-//#include <algorithm>
+#include <algorithm>
 #include <vector>
 
 
 
 int zuper_modulus(int a,int b);
 
-float delta_H(const std::vector<std::vector<float>> &input_matrix, int x, int y);
+float delta_H(const std::vector<float> &input_matrix, int x, int y,int n);
 
-void evolve_model(std::vector<std::vector<float>> &input_matrix,float Beta,int steps);
+void evolve_model(std::vector<float> &input_matrix,float Beta,int steps,int n);
 
+void regrow_grass(std::vector<float>& lattice, float regrowth_rate);
 
-void initialize_model(std::vector<std::vector<float>> &input_matrix);
+void grow_berries(std::vector<float> &input_matrix,int size,int n);
+void initialize_model(std::vector<float> &input_matrix);
 #endif
