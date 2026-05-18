021257938686f59c555c72bc37a618c75eaa5f69
seabornema <seabornem@proton.me>
2026-05-18 13:46:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 #include <header/creature.h>
 #include <header/randomizers.h>
 #include <header/config.h>
+#include <header/quadtrees.h>
 #include <header/cl_buffers.h>
 #include <header/cl_programs.h>
 #include <header/gl_helpers.h>
@@ -43,7 +44,7 @@ vector<float> lattice(mainConfig.worldConfig.L_sq());
 
 vector<Creature> initialize_creatures(MainConfig &config) {
     vector<Creature> temp; 
-  for(int i =0; i<config.worldConfig.L;i++) {
+  for(int i =0; i<config.worldConfig.initial_creature_count;i++) {
     temp.push_back(random_creature(mainConfig));
   }
   return temp;
@@ -51,7 +52,7 @@ vector<Creature> initialize_creatures(MainConfig &config) {
 
 
 void create_fullpass_output(vector<float> &destination,cl_mem &true_output_buffer,cl_command_queue &queue,int csize){
-destination.resize(4*csize);
+destination.resize(8*csize);
 clEnqueueReadBuffer(queue, true_output_buffer, CL_TRUE, 0, sizeof(float) * destination.size(), destination.data(), 0, NULL, NULL);
 }
 
@@ -59,6 +60,7 @@ int main()
 {
   int width = mainConfig.graphicsConfig.width;
   int height = mainConfig.graphicsConfig.height;
+  int L = mainConfig.worldConfig.L;
   //settings 
   //
 
@@ -94,14 +96,15 @@ cl_mem output_color_buffer;
 cl_mem weight_buffer;
 cl_mem brain_input_buffer;
 cl_mem true_output_buffer;
+cl_mem cost_function_buffer;
 
 init_opencl_color_buffers(position_input_buffer,output_color_buffer, lattice_input_buffer, context, err,mainConfig);
-init_opencl_brain_buffers(brain_input_buffer,true_output_buffer, weight_buffer, context, err,mainConfig);
+init_opencl_brain_buffers(brain_input_buffer,true_output_buffer, weight_buffer,cost_function_buffer ,context, err,mainConfig,queue_gpu);
 
 creatures =  initialize_creatures(mainConfig);
 
 initialize_model(lattice); 
-evolve_model(lattice, 1.0f,pow(mainConfig.worldConfig.L,2.5),mainConfig.worldConfig.L); // generate terrain
+evolve_model(lattice, 4.0f,pow(L,3),L); // generate terrain
                                          //
                                          //
                                          //
@@ -111,7 +114,7 @@ vector<int> init_index_array;
 position_array_builder(creatures,init_position_array,init_index_array);
 
 fullpass(input_kernel,neural_network_kernel,devices[0],err,queue_gpu,position_input_buffer
-    ,lattice_input_buffer,output_color_buffer,weight_buffer,brain_input_buffer
+    ,lattice_input_buffer,output_color_buffer,weight_buffer,brain_input_buffer,cost_function_buffer
     ,true_output_buffer,init_position_array,lattice,init_index_array,creatures,mainConfig);
 
 
@@ -149,11 +152,11 @@ glfwSwapInterval(0); //vsync 0=off 1 = on
     Shader LinesShader("include/shader/lines.vert", "include/shader/lines.frag");
     Shader CircleShader("include/shader/circle.vert", "include/shader/circle.frag");
 
+
+    Camera main_camera(1.0f,width, height, glm::vec3(0.0f,0.0f,0.0f));
+
+
     vector<float> vertices = quad_generator(mainConfig); 
-
-    Camera main_camera((width/(mainConfig.worldConfig.L*mainConfig.worldConfig.tile_size)),width, height, glm::vec3(-(float)width/2.0f, -(float)height/2.0f, 0.0f));
-
-
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -170,8 +173,8 @@ glfwSwapInterval(0); //vsync 0=off 1 = on
     unsigned int texture;
     glGenTextures(1, &texture);   
     glBindTexture(GL_TEXTURE_2D, texture);
-    vector<unsigned char> texData;
-      makeRGBTexture(texData,lattice,width,height,main_camera,mainConfig);
+    vector<unsigned char> texData(3*width*height);
+    makeRGBTexture(texData,lattice,width,height,main_camera,mainConfig);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, texData.data());
     
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -192,34 +195,36 @@ glfwSwapInterval(0); //vsync 0=off 1 = on
     generate_creature_buffers(VAO_circle,VBO_circle,VAO_lines,VBO_lines,creatures.size(),init_position_array);
 
   
-
-
+    grow_berries(lattice,L,10);
     //rendering loop!!! 
     //
 
     double previousTime = glfwGetTime();
     int frameCount = 0;
+      
 
-    grow_berries(lattice,mainConfig.worldConfig.L,1);
+
+
+
+    vector<float> position_array;
+      vector<int> index_array;
+
     while (!glfwWindowShouldClose(window))
     {
+
+      quadtree qt = build(creatures.begin(), creatures.end());
+      collide_all(qt, creatures.begin(), creatures.end());
       //background
       //
       //
-      
+      //
+      regrow_grass(lattice,0.00026f); 
 
-regrow_grass(lattice,0.00001f); 
       double currentTime = glfwGetTime();
       frameCount++;
 
 
 
-      if (currentTime - previousTime >= 1.0) {
-          cout << "FPS: " << frameCount << endl;
-          cout<< "Creatures: "<<creatures.size()<<endl;
-        frameCount = 0;
-        previousTime = currentTime;
-      }
 
       processInput(window);
 
@@ -234,6 +239,8 @@ regrow_grass(lattice,0.00001f);
 
       //background
       //
+
+     
       ourShader.use();
       glActiveTexture(GL_TEXTURE0);
       makeRGBTexture(texData,lattice,width,height,main_camera,mainConfig);
@@ -242,6 +249,7 @@ regrow_grass(lattice,0.00001f);
       main_camera.Matrix(ourShader,"camMatrix");
       glUniform1i(glGetUniformLocation(ourShader.ID, "tex"), 0);
       glBindVertexArray(VAO);
+
 
       glBindTexture(GL_TEXTURE_2D, texture);
       glTexSubImage2D(GL_TEXTURE_2D,0,0,0,width,height,GL_RGB,GL_UNSIGNED_BYTE,texData.data());
@@ -253,22 +261,32 @@ regrow_grass(lattice,0.00001f);
 
 
 
-      vector<float> position_array;
-      vector<int> index_array;
+
+
+
+
+      for(int i =creatures.size()-1;i>= 0;i--){
+        float ec = creatures[i].energy;
+        if(ec <= 0) {
+          creatures.erase(creatures.begin()+i);
+          continue;
+        }
+
+      }
       position_array_builder(creatures,position_array,index_array);
-    fullpass(input_kernel,neural_network_kernel,devices[0],err,queue_gpu,position_input_buffer,lattice_input_buffer,output_color_buffer,weight_buffer,brain_input_buffer,
-    true_output_buffer,position_array,lattice,index_array,creatures,mainConfig);
-   vector<float> instructions;
-   create_fullpass_output(instructions,true_output_buffer,queue_gpu,creatures.size());
+      fullpass(input_kernel,neural_network_kernel,devices[0],err,queue_gpu,position_input_buffer,lattice_input_buffer,output_color_buffer,weight_buffer,brain_input_buffer,cost_function_buffer,
+      true_output_buffer,position_array,lattice,index_array,creatures,mainConfig);
+      vector<float> instructions;
+      create_fullpass_output(instructions,true_output_buffer,queue_gpu,creatures.size());
+
+
 
       vector<float> creature_body_vertices;
       vector<Creature> new_creatures;
-      int csize = creatures.size();
-      for(int i =csize-1;i>= 0;i--){
-
-        int j = (4*i);
+      for(int i =0;i<creatures.size();i++) {
+        
+        int j = (8*i);
         Creature &C = creatures[i];
-
         vector<float> inputs(4);
         inputs[0] = instructions[j];
         inputs[1] = instructions[j+1];
@@ -276,33 +294,33 @@ regrow_grass(lattice,0.00001f);
         inputs[3] = instructions[j+3];
 
         C.time_evolve(inputs,new_creatures,lattice,mainConfig.worldConfig.L);
-    
-        if(C.energy <= 0) {
-          creatures.erase(creatures.begin()+i);
-          continue;
-        }
+   
       
         vector<float> this_triangle = creature_body_model_generator(C,main_camera);
         creature_body_vertices.insert(creature_body_vertices.end(),this_triangle.begin(),this_triangle.end());
-
       }
-      creatures.insert(creatures.end(),new_creatures.begin(),new_creatures.end());
+
 
       CircleShader.use();
 
       main_camera.Matrix(CircleShader,"camMatrix");
 
-      //creature loop
-      //
 
       glBindBuffer(GL_ARRAY_BUFFER, VBO_circle);
-      glBufferSubData(GL_ARRAY_BUFFER, 0, creature_body_vertices.size()*sizeof(float), creature_body_vertices.data());
+      glBufferData(GL_ARRAY_BUFFER, creature_body_vertices.size()*sizeof(float), creature_body_vertices.data(),GL_DYNAMIC_DRAW);
       glBindVertexArray(VAO_circle);
       glDrawArrays(GL_TRIANGLES, 0, (creature_body_vertices.size()/10));
+     
 
 
 
 
+
+      creatures.insert(creatures.end(),new_creatures.begin(),new_creatures.end());
+      if(mainConfig.worldConfig.random_spawn_rate > random_range(0,1.0)) {
+        cout << "random_spawn" <<endl;
+        creatures.push_back(random_creature(mainConfig));
+      }
 
 
 
@@ -325,17 +343,23 @@ regrow_grass(lattice,0.00001f);
       vector<float> eye_array;
       gl_position_array_builder(creatures,eye_array,main_camera);
       glBindBuffer(GL_ARRAY_BUFFER, VBO_lines);
-      glBufferSubData(GL_ARRAY_BUFFER, 0, eye_array.size()*sizeof(float), eye_array.data());
       glBindVertexArray(VAO_lines);
+      glBufferData(GL_ARRAY_BUFFER, eye_array.size()*sizeof(float), eye_array.data(),GL_DYNAMIC_DRAW);
       glDrawArrays(GL_LINES, 0, eye_array.size()/2);
 
 
 
-    generate_creature_buffers(VAO_circle, VBO_circle, VAO_lines, VBO_lines, creatures.size(), position_array);
+      //  generate_creature_buffers(VAO_circle, VBO_circle, VAO_lines, VBO_lines, creatures.size(), position_array);
 
+      if (currentTime - previousTime >= 1.0) {
+          cout << "FPS: " << frameCount << endl;
+          cout<< "Creatures: "<<creatures.size()<<endl;
+        frameCount = 0;
+        previousTime = currentTime;
+      }
       glfwSwapBuffers(window);
       glfwPollEvents();
-      }
+    }
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
